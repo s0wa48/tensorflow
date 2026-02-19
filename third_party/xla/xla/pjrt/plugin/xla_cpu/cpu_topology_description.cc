openxla/xla

[XLA:CPU] Put TargetMachineOptions into CpuTopology
Provides all the information XLA:CPU passes to LLVM so that we can easily cross compile

PiperOrigin-RevId: 862188177

xla/pjrt/plugin/xla_cpu/cpu_topology_description.cc
@@ -26,6 +26,7 @@ limitations under the License.
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
+#include "xla/backends/cpu/target_machine_options.h"
 #include "xla/layout.h"
 #include "xla/layout_util.h"
 #include "xla/pjrt/pjrt_common.h"
@@ -39,6 +40,22 @@ limitations under the License.
 #include "xla/tsl/lib/strings/proto_serialization.h"
 
 namespace xla {
+
+/*static*/ PjRtPlatformId CpuPlatformId() { return xla::CpuId(); }
+
+/*static*/ absl::string_view CpuPlatformName() { return xla::CpuName(); }
+
+/*static*/ absl::string_view CpuPlatformVersion() { return xla::CpuName(); }
+
+CpuTopologyDescription::CpuTopologyDescription(
+    PjRtPlatformId platform_id, absl::string_view platform_name,
+    absl::string_view platform_version,
+    const std::vector<::xla::CpuTopology::CpuDevice>& cpu_devices,
+    const ::xla::cpu::TargetMachineOptions& target_machine_options)
+    : platform_id_(platform_id),
+      platform_name_(platform_name),
+      platform_version_(platform_version),
+      cpu_topology_(cpu_devices, target_machine_options) {}
 
 absl::StatusOr<Layout> CpuTopologyDescription::GetDefaultLayout(
     PrimitiveType element_type, absl::Span<const int64_t> dims) const {
@@ -99,14 +116,13 @@ CpuTopologyDescription::FromProto(
   }
   CpuTopologyProto cpu_topology_proto;
   proto.platform_specific_topology().UnpackTo(&cpu_topology_proto);
-  auto cpu_topology = std::shared_ptr<const CpuTopology>(
-      CpuTopology::FromProto(cpu_topology_proto));
+  auto cpu_topology = CpuTopology::FromProto(cpu_topology_proto);
   std::vector<xla::CpuTopology::CpuDevice> cpu_devices;
   cpu_devices.assign(cpu_topology->devices().begin(),
                      cpu_topology->devices().end());
   return std::make_unique<CpuTopologyDescription>(
       proto.platform_id(), proto.platform_name(), proto.platform_version(),
-      cpu_devices, cpu_topology->machine_attributes());
+      cpu_devices, cpu_topology->target_machine_options());
 }
 
 }  // namespace xla

s0wa48/tensorflow

third_party/xla/xla/pjrt/plugin/xla_cpu/cpu_topology_description.cc
@@ -38,8 +38,23 @@ limitations under the License.
 #include "xla/shape.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/lib/strings/proto_serialization.h"
+#include "xla/tsl/platform/status_macros.h"
 
 namespace xla {
+
+/*static*/ PjRtPlatformId CpuPlatformId() { return xla::CpuId(); }
+
+/*static*/ absl::string_view CpuPlatformName() { return xla::CpuName(); }
+
+/*static*/ absl::string_view CpuPlatformVersion() { return xla::CpuName(); }
+
+CpuTopologyDescription::CpuTopologyDescription(
+    PjRtPlatformId platform_id, absl::string_view platform_name,
+    absl::string_view platform_version, const CpuTopology& cpu_topology)
+    : platform_id_(platform_id),
+      platform_name_(platform_name),
+      platform_version_(platform_version),
+      cpu_topology_(cpu_topology) {}
 
 absl::StatusOr<Layout> CpuTopologyDescription::GetDefaultLayout(
     PrimitiveType element_type, absl::Span<const int64_t> dims) const {
@@ -100,14 +115,12 @@ CpuTopologyDescription::FromProto(
   }
   CpuTopologyProto cpu_topology_proto;
   proto.platform_specific_topology().UnpackTo(&cpu_topology_proto);
-  auto cpu_topology = std::shared_ptr<const CpuTopology>(
-      CpuTopology::FromProto(cpu_topology_proto));
+  ASSIGN_OR_RETURN(auto cpu_topology,
+                   CpuTopology::FromProto(cpu_topology_proto));
   std::vector<xla::CpuTopology::CpuDevice> cpu_devices;
-  cpu_devices.assign(cpu_topology->devices().begin(),
-                     cpu_topology->devices().end());
   return std::make_unique<CpuTopologyDescription>(
       proto.platform_id(), proto.platform_name(), proto.platform_version(),
-      cpu_devices, cpu_topology->machine_attributes());
+      *cpu_topology);
 }
 
 }  // namespace xla