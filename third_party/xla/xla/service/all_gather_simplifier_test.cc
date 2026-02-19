zacharydenton/xla-1

Generalize all gather dynamic slice.
PiperOrigin-RevId: 864676487

xla/service/all_gather_simplifier_test.cc
@@ -15,8 +15,11 @@ limitations under the License.
 
 #include "xla/service/all_gather_simplifier.h"
 
+#include <iostream>
 #include <memory>
 
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
@@ -81,6 +84,83 @@ test {
   auto result = ag_simplifier.Run(module.get());
   ASSERT_TRUE(result.ok()) << result.status();
   ASSERT_FALSE(result.value());
+}
+
+TEST_F(AllGatherSimplifierTest, GeneralizedDynamicSliceCancellation) {
+  const absl::string_view kModuleStr = R"hlo(
+HloModule m
+
+ENTRY test {
+ p0 = f32[5, 10] parameter(0)
+ all-gather = f32[20, 10] all-gather(p0), replica_groups={{0, 1, 2, 3}}, dimensions={0}, use_global_device_ids=true, channel_id=1
+  partition-id = u32[] partition-id()
+  index = s32[] convert(partition-id)
+  shard_size = s32[] constant(5)
+  device_base_idx = s32[] multiply(index, shard_size)
+  c0 = s32[] constant(0)
+  ds.0 = f32[4,10] dynamic-slice(all-gather, device_base_idx, c0), dynamic_slice_sizes={4,10}
+  const_1 = s32[] constant(1)
+  offset_ds.1 = s32[] add(device_base_idx, const_1)
+  ds.1 = f32[4,10] dynamic-slice(all-gather, offset_ds.1, c0), dynamic_slice_sizes={4,10}
+  ROOT tuple = (f32[4,10], f32[4,10]) tuple(ds.0, ds.1)
+}
+)hlo";
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(
+                                           kModuleStr, /*replica_count=*/4));
+  module->mutable_config().set_use_spmd_partitioning(true);
+  AllGatherSimplifier ag_simplifier;
+  ASSERT_TRUE(ag_simplifier.Run(module.get()).value());
+  // Expect dynamic slice operand is a parameter
+  HloInstruction* ds_0 =
+      module->entry_computation()->GetInstructionWithName("ds.0");
+  EXPECT_THAT(ds_0->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_1 =
+      module->entry_computation()->GetInstructionWithName("ds.1");
+  EXPECT_THAT(ds_1->operand(0), GmockMatch(m::Parameter(0)));
+}
+
+TEST_F(AllGatherSimplifierTest, GeneralizedDynamicSliceCancellation2) {
+  const absl::string_view kModuleStr = R"hlo(
+HloModule m
+
+ENTRY test {
+  %c0 = s32[]{:T(128)} constant(0)
+  %c2 = s32[]{:T(128)} constant(2)
+  %c14 = s32[]{:T(128)} constant(14)
+  %c2048 = s32[]{:T(128)} constant(2048)
+  %c3072 = s32[]{:T(128)} constant(3072)
+
+  %replica-id = u32[]{:T(128)} partition-id()
+  %convert = s32[]{:T(128)} convert(%replica-id)
+  %mul = s32[]{:T(128)} multiply(%convert, %c2)
+  %clamp = s32[]{:T(128)} clamp(%c0, %mul, %c14)
+
+  %p0 = bf16[2,4096,128,192]{2,3,1,0:T(8,128)(2,1)} parameter(0)
+
+  %all-gather = bf16[16,4096,128,192]{2,3,1,0:T(8,128)(2,1)} all-gather(%p0), channel_id=1, replica_groups=[1,8]<=[8], dimensions={0}, use_global_device_ids=true
+
+  %dynamic-slice.4 = bf16[2,4096,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %mul, %c0, %c0, %c0), dynamic_slice_sizes={2,4096,128,192}
+
+  %dynamic-slice.30 = bf16[2,1024,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %clamp, %c2048, %c0, %c0), dynamic_slice_sizes={2,1024,128,192}
+
+  %dynamic-slice.31 = bf16[2,1024,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %clamp, %c3072, %c0, %c0), dynamic_slice_sizes={2,1024,128,192}
+  ROOT %copy.269 = bf16[2,1024,128,192]{1,3,2,0:T(8,128)(2,1)} copy(%dynamic-slice.31)
+}
+)hlo";
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(
+                                           kModuleStr, /*replica_count=*/8));
+  module->mutable_config().set_use_spmd_partitioning(true);
+  AllGatherSimplifier ag_simplifier;
+  ASSERT_TRUE(ag_simplifier.Run(module.get()).value());
+  HloInstruction* ds_31 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.31");
+  EXPECT_THAT(ds_31->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_30 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.30");
+  EXPECT_THAT(ds_30->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_4 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.4");
+  EXPECT_THAT(ds_4->operand(0), GmockMatch(m::Parameter(0)));
 }
 
 }  // namespace

s0wa48/tensorflow

third_party/xla/xla/service/all_gather_simplifier_test.cc
@@ -83,5 +83,84 @@ test {
   ASSERT_FALSE(result.value());
 }
 
+TEST_F(AllGatherSimplifierTest, GeneralizedDynamicSliceCancellation) {
+  const absl::string_view kModuleStr = R"hlo(
+HloModule m
+
+ENTRY test {
+ p0 = f32[5, 10] parameter(0)
+ all-gather = f32[20, 10] all-gather(p0), replica_groups={{0, 1, 2, 3}}, dimensions={0}, use_global_device_ids=true, channel_id=1
+  replica-id = u32[] replica-id()
+  index = s32[] convert(replica-id)
+  shard_size = s32[] constant(5)
+  device_base_idx = s32[] multiply(index, shard_size)
+  c0 = s32[] constant(0)
+  ds.0 = f32[4,10] dynamic-slice(all-gather, device_base_idx, c0), dynamic_slice_sizes={4,10}
+  const_1 = s32[] constant(1)
+  offset_ds.1 = s32[] add(device_base_idx, const_1)
+  ds.1 = f32[4,10] dynamic-slice(all-gather, offset_ds.1, c0), dynamic_slice_sizes={4,10}
+  ROOT tuple = (f32[4,10], f32[4,10]) tuple(ds.0, ds.1)
+}
+)hlo";
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(
+                                           kModuleStr, /*replica_count=*/4));
+  module->mutable_config().set_use_spmd_partitioning(true);
+  AllGatherSimplifier ag_simplifier;
+  TF_ASSERT_OK_AND_ASSIGN(bool changed, ag_simplifier.Run(module.get()));
+  ASSERT_TRUE(changed);
+  // Expect dynamic slice operand is a parameter
+  HloInstruction* ds_0 =
+      module->entry_computation()->GetInstructionWithName("ds.0");
+  EXPECT_THAT(ds_0->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_1 =
+      module->entry_computation()->GetInstructionWithName("ds.1");
+  EXPECT_THAT(ds_1->operand(0), GmockMatch(m::Parameter(0)));
+}
+
+TEST_F(AllGatherSimplifierTest, GeneralizedDynamicSliceCancellation2) {
+  const absl::string_view kModuleStr = R"hlo(
+HloModule m
+
+ENTRY test {
+  %c0 = s32[]{:T(128)} constant(0)
+  %c2 = s32[]{:T(128)} constant(2)
+  %c14 = s32[]{:T(128)} constant(14)
+  %c2048 = s32[]{:T(128)} constant(2048)
+  %c3072 = s32[]{:T(128)} constant(3072)
+
+  %replica-id = u32[]{:T(128)} replica-id()
+  %convert = s32[]{:T(128)} convert(%replica-id)
+  %mul = s32[]{:T(128)} multiply(%convert, %c2)
+  %clamp = s32[]{:T(128)} clamp(%c0, %mul, %c14)
+
+  %p0 = bf16[2,4096,128,192]{2,3,1,0:T(8,128)(2,1)} parameter(0)
+
+  %all-gather = bf16[16,4096,128,192]{2,3,1,0:T(8,128)(2,1)} all-gather(%p0), channel_id=1, replica_groups=[1,8]<=[8], dimensions={0}, use_global_device_ids=true
+
+  %dynamic-slice.4 = bf16[2,4096,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %mul, %c0, %c0, %c0), dynamic_slice_sizes={2,4096,128,192}
+
+  %dynamic-slice.30 = bf16[2,1024,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %clamp, %c2048, %c0, %c0), dynamic_slice_sizes={2,1024,128,192}
+
+  %dynamic-slice.31 = bf16[2,1024,128,192]{2,3,1,0:T(8,128)(2,1)} dynamic-slice(%all-gather, %clamp, %c3072, %c0, %c0), dynamic_slice_sizes={2,1024,128,192}
+  ROOT %copy.269 = bf16[2,1024,128,192]{1,3,2,0:T(8,128)(2,1)} copy(%dynamic-slice.31)
+}
+)hlo";
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(
+                                           kModuleStr, /*replica_count=*/8));
+  module->mutable_config().set_use_spmd_partitioning(true);
+  AllGatherSimplifier ag_simplifier;
+  TF_ASSERT_OK_AND_ASSIGN(bool changed, ag_simplifier.Run(module.get()));
+  ASSERT_TRUE(changed);
+  HloInstruction* ds_31 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.31");
+  EXPECT_THAT(ds_31->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_30 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.30");
+  EXPECT_THAT(ds_30->operand(0), GmockMatch(m::Parameter(0)));
+  HloInstruction* ds_4 =
+      module->entry_computation()->GetInstructionWithName("dynamic-slice.4");
+  EXPECT_THAT(ds_4->operand(0), GmockMatch(m::Parameter(0)));
+}
+
 }  // namespace
 }  // namespace xla